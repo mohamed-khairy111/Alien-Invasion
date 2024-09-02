ea0f66ec13e8caa21fe1b62ba3b83b1372745f35
mohamed khairy <mohamed khairy>
2024-09-02 19:06:12 +0300

    ...

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,4 @@
-import sys
+""" import sys
 
 import pygame
 
@@ -6,10 +6,9 @@ from setting import Setting
 from ship import Ship
 
 class AlienInvasion:
-    """Overall class to manage game assets and behavior"""
-
+  
     def __init__(self):
-        """initialize the game, and create game resources"""
+       
         pygame.init()
 
         self.clock  = pygame.time.Clock()
@@ -25,7 +24,7 @@ class AlienInvasion:
         self.bg_color = (189, 170, 130)
 
     def run_game(self):
-        """Start the main loop for the game"""
+       
         while True:
             #Watch for keyboard and mouse events.
             for event in pygame.event.get():
@@ -42,5 +41,51 @@ class AlienInvasion:
 
 if __name__ == '__main__':
     #Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game() """
+
+
+import sys
+
+import pygame
+
+from setting import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60)
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
